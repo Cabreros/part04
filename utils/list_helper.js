b52f8b0f0c42a8326dfbcc593723782c6ddd66ce
michael <cabreros17@gmail.com>
2022-04-21 21:21:54 -0400

    4.4 helper functions and unit tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,12 @@
 const dummy = () => 1;
 
+const totalLikes = (blogPosts) => {
+  var total = 0;
+  blogPosts.forEach((arg) => (total += arg.likes));
+  return total;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
