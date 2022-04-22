b4a96c1bdf130a262e281ffe52f93e456eef4c60
michael <cabreros17@gmail.com>
2022-04-22 13:50:25 -0400

    4.5 helper functions and unit testing

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,12 +1,39 @@
+const blog = require("../models/blog");
+
 const dummy = () => 1;
 
 const totalLikes = (blogPosts) => {
-  var total = 0;
-  blogPosts.forEach((arg) => (total += arg.likes));
+  let total = 0;
+  blogPosts.forEach((post) => (total += post.likes));
   return total;
+};
+
+const favoriteBlog = (blogPosts) => {
+  let mostLikes = {};
+  let maxLikes = 0;
+  console.log(blogPosts);
+
+  if (blogPosts === {}) {
+    return {};
+  } else {
+    for (let i = 0; i < blogPosts.length; i++) {
+      let post = blogPosts[i];
+      if (post.likes > maxLikes) {
+        mostLikes = post;
+        maxLikes = post.likes;
+      }
+    }
+
+    return {
+      title: mostLikes.title,
+      author: mostLikes.author,
+      likes: mostLikes.likes,
+    };
+  }
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
